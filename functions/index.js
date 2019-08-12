d30c1ebdac0dd1299af4c3f80f163e7ede46d7d5
Rejohnbd <rejohn.cse@gmail.com>
2019-08-12 18:48:29 +0600

    Firebase Auth User Create

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,23 @@
 const functions = require('firebase-functions');
-
 const admin = require('firebase-admin');
+const express = require('express');
+
+const app = express();
+
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyAuOaK0t2Yc21aqQ07mE3CP5uZU3saSp14",
+    authDomain: "socialapp-2019.firebaseapp.com",
+    databaseURL: "https://socialapp-2019.firebaseio.com",
+    projectId: "socialapp-2019",
+    storageBucket: "socialapp-2019.appspot.com",
+    messagingSenderId: "829866265222",
+    appId: "1:829866265222:web:a068b14663558b13"
+}
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
 app.get('/screams', (req, res) => {
     admin
@@ -24,8 +37,8 @@ app.get('/screams', (req, res) => {
             });
             return res.json(screams);
         })
-        .catch(err => console.error(err))
-})
+        .catch(err => console.error(err));
+});
 
 
 
@@ -40,12 +53,35 @@ app.post('/scream', (req, res) => {
         .collection('screams')
         .add(newScream)
         .then((doc) => {
-            res.json({ message: `document ${doc.id} created successfully` })
+            res.json({ message: `document ${doc.id} created successfully` });
         })
         .catch(err => {
-            res.status(500).json({ error: 'something went wrong' })
-            console.error(err)
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+        });
+});
+
+// Signup Route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassord: req.body.confirmPassord,
+        handle: req.body.handle
+    };
+
+    //Validate data
+    
+
+    // Firebase Auth Create User
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return res.status(201).json({ message: `User ${data.user.uid} signed up successfully` });
         })
-})
+        .catch( err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
+});
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
