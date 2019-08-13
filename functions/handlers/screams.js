542731484b060826785765f04f607aedb3773429
Rejohnbd <rejohn.cse@gmail.com>
2019-08-14 01:07:14 +0600

    Fix comment increament

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -94,6 +94,9 @@ exports.commentOnScream = (req, res) => {
             if(!doc.exists) {
                 return res.status(400).json({ error: 'Scream not found' });
             }
+            return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+        })
+        .then(() => {
             return admin.firestore().collection('comments').add(newComment);
         })
         .then(() => {
