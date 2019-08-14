0d21b7f095276c93dccd4ded123b9ed2554c105f
Rejohnbd <rejohn.cse@gmail.com>
2019-08-14 17:46:05 +0600

    Ready for deploy

--- functions/utils/firebaseConfig.js
+++ functions/utils/firebaseConfig.js
@@ -1,9 +1,3 @@
 module.exports = {
-    apiKey: "AIzaSyAuOaK0t2Yc21aqQ07mE3CP5uZU3saSp14",
-    authDomain: "socialapp-2019.firebaseapp.com",
-    databaseURL: "https://socialapp-2019.firebaseio.com",
-    projectId: "socialapp-2019",
-    storageBucket: "socialapp-2019.appspot.com",
-    messagingSenderId: "829866265222",
-    appId: "1:829866265222:web:a068b14663558b13"
+    
 }
